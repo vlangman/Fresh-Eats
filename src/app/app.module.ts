3a4a81927c151b1b19536de9a1769b0feaf2d9d8
vlangman <vlangman00@gmail.com>
2018-09-24 19:57:28 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+  	NgbModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
